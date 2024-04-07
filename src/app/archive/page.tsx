38c9a3b2c17130ec71bc4a7c3197ad295c47b1f9
rustedmiind <alisolimanworks@gmail.com>
2024-04-07 17:46:49 +0200

    Archive Page

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -1,11 +1,14 @@
-import {
-  Stack,
-} from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import ProjectsTable from "./_Projects/Table";
+import BackToHomeBtn from "@/components/BackToHomeBtn";
 
 function ArchivePage() {
   return (
     <Stack>
+      <BackToHomeBtn />
+      <Typography variant={"h3"} fontWeight={700} gutterBottom>
+        All Projects
+      </Typography>
       <ProjectsTable />
     </Stack>
   );
