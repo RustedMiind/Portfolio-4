c60d2478b183734ae1fd4be7368e492fe1cf5ce6
rustedmiind <alisolimanworks@gmail.com>
2024-04-09 20:48:57 +0200

    Added Forward Link For projects archive in home page

--- src/app/(home)/_sections/Projects/index.tsx
+++ src/app/(home)/_sections/Projects/index.tsx
@@ -1,6 +1,7 @@
 import { Stack } from "@mui/material";
 import ProjectCard from "./_ProjectCard.tsx";
-import SectionContainer from "@/components/SectionContainer/index.tsx";
+import SectionContainer from "@/components/SectionContainer/index";
+import ForwardLink from "@/components/Links/ForwardLink";
 
 function ProjectsSection() {
   return (
@@ -9,6 +10,7 @@ function ProjectsSection() {
       <ProjectCard />
       <ProjectCard />
       <ProjectCard />
+      <ForwardLink title="View Full Project Archive" href="/archive" />
     </SectionContainer>
   );
 }
