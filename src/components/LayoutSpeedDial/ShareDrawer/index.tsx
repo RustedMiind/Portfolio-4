91af4461e9f7a35e22a94e9ea262639ce7942b94
rustedmiind <alisolimanworks@gmail.com>
2024-04-15 21:04:53 +0200

    Share url, title and hastags

--- src/components/LayoutSpeedDial/ShareDrawer/index.tsx
+++ src/components/LayoutSpeedDial/ShareDrawer/index.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { variablesAtom } from "@/jotai/atoms/Variables";
 import {
   Container,
   Stack,
   SwipeableDrawer,
   SwipeableDrawerProps,
 } from "@mui/material";
+import { useAtomValue } from "jotai";
+import { useMemo } from "react";
 
 import {
   FacebookShare,
@@ -18,28 +21,41 @@ import {
   TelegramShare,
   FacebookMessengerShare,
 } from "react-share-lite";
-const shareDetails = {
-  title: "See This amazing portfolio of Ali Soliman",
-  url: "https://www.npmjs.com/package/react-share-lite",
-  round: true,
-};
-const hashtags =
-  "#web #web_developer #frontend #backend #fullstack #portfolio #react #nestjs #nextjs";
 
 function ShareDrawer({ ...props }: Props) {
+  const url = useMemo(() => window.location.origin, []);
+
+  const variables = useAtomValue(variablesAtom);
+  const shareDetails = {
+    title: variables?.share_title,
+    url,
+    round: true,
+    windowPosition: "screenCenter",
+    windowHeight: 720,
+    windowWidth: 1280,
+  } as const;
+  const hashtags = variables?.share_hashtags?.replaceAll(" ", "").split("#");
+
   return (
     <SwipeableDrawer anchor="bottom" {...props}>
       <Container maxWidth="sm">
-        <Stack direction="row" gap={1} py={{ xs: 4, md: 8 }} flexWrap="wrap">
-          <FacebookShare {...shareDetails} quote="Hello" hashtag={hashtags} />
+        <Stack
+          key={`${hashtags?.join()} ${variables?.share_title}`}
+          direction="row"
+          gap={1}
+          py={{ xs: 4, md: 8 }}
+          flexWrap="wrap"
+        >
+          <FacebookShare
+            {...shareDetails}
+            quote={shareDetails.title}
+            hashtag={hashtags?.join(" #")}
+          />
           <EmailShare {...shareDetails} subject={shareDetails.title} />
           <LinkedinShare {...shareDetails} />
           <WhatsappShare {...shareDetails} />
           <RedditShare {...shareDetails} />
-          <TwitterShare
-            {...shareDetails}
-            hashtags={hashtags.split("#").slice(1)}
-          />
+          <TwitterShare {...shareDetails} hashtags={hashtags} />
           <TumblrShare {...shareDetails} caption={shareDetails.title} />
           <TelegramShare {...shareDetails} />
         </Stack>
