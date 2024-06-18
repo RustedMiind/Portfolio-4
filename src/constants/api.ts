dd98907e9281d27c79ae8779efc89dfb36761c08
rustedmiind <alisolimanworks@gmail.com>
2024-06-18 03:22:30 +0300

    server domain to raw ip address

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,5 +1,5 @@
 function api(path?: string) {
-  return `http://server.alisoliman.net/${path || ""}`;
+  return `http://77.37.120.134:8889/${path || ""}`;
   return `http://localhost:3333/${path || ""}`;
 }
 
